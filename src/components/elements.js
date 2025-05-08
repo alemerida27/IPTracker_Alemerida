b11ba1cb2f67e5af13aadb6251b6e74468d991a5
Kaom123 <78056145+Kaom123@users.noreply.github.com>
2025-05-08 17:31:52 -0500

    ESLINT & PRETTIER

--- src/components/elements.js
+++ src/components/elements.js
@@ -1,81 +1,77 @@
-import '..styles/style.css'
+import "..styles/style.css";
 
-
-
-function Button(texto, clase){
-let boton = document.createElement("button")
-boton.textContent = texto
-if(clase !== ""){
-    boton.classList.add(clase)
-    return boton
-}else {
-    return boton
-}
+function Button(texto, clase) {
+  let boton = document.createElement("button");
+  boton.textContent = texto;
+  if (clase !== "") {
+    boton.classList.add(clase);
+    return boton;
+  } else {
+    return boton;
+  }
 }
 
-function Parrafo(texto, clase){
-    let parrafo = document.createElement("p")
-parrafo.textContent = texto
-if(clase !== ""){
-    parrafo.classList.add(clase)
-    return parrafo
-}else {
-    return parrafo
-}
-}
-
-function Titulo(texto, clase){
-    let title = document.createElement("h1")
-title.textContent = texto
-if(clase !== ""){
-    title.classList.add(clase)
-    return title
-}else {
-    return title
-}
+function Parrafo(texto, clase) {
+  let parrafo = document.createElement("p");
+  parrafo.textContent = texto;
+  if (clase !== "") {
+    parrafo.classList.add(clase);
+    return parrafo;
+  } else {
+    return parrafo;
+  }
 }
 
-function Div(clase){
-let div = document.createElement("div")
-if(clase !== ""){
-    div.classList.add(clase)
-    return div
-}else {
-    return div
-}
-}
-
-function Span(clase){
-    let span = document.createElement("span")
-    if(clase !== ""){
-        span.classList.add(clase)
-        return span
-    }else {
-        return span
-    }
-    }
-
-function Section(clase){
-    let section = document.createElement("section")
-    if(clase !== ""){
-        section.classList.add(clase)
-        return section
-    }else {
-        return section
-    }
+function Titulo(texto, clase) {
+  let title = document.createElement("h1");
+  title.textContent = texto;
+  if (clase !== "") {
+    title.classList.add(clase);
+    return title;
+  } else {
+    return title;
+  }
 }
 
-function Image(src, clase){
-    let image = document.createElement("img")
-    image.src = src
-    if(clase !== ""){
-        image.classList.add(clase)
-        return image
-    }else {
-        return image
-    }
+function Div(clase) {
+  let div = document.createElement("div");
+  if (clase !== "") {
+    div.classList.add(clase);
+    return div;
+  } else {
+    return div;
+  }
 }
 
+function Span(clase) {
+  let span = document.createElement("span");
+  if (clase !== "") {
+    span.classList.add(clase);
+    return span;
+  } else {
+    return span;
+  }
+}
 
+function Section(clase) {
+  let section = document.createElement("section");
+  if (clase !== "") {
+    section.classList.add(clase);
+    return section;
+  } else {
+    return section;
+  }
+}
 
-export { Button, Parrafo, Titulo, Div, Section, Image, Span};
+function Image(src, clase) {
+  let image = document.createElement("img");
+  image.src = src;
+  if (clase !== "") {
+    image.classList.add(clase);
+    return image;
+  } else {
+    return image;
+  }
+}
+
+export { Button, Parrafo, Titulo, Div, Section, Image, Span };
